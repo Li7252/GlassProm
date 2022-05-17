6afa6525aa2db612726f2e19e98f0b863fc105da
Verkunder <verkunder614@gmail.com>
2022-05-17 22:45:32 +0300

    feat(Gallery) Сделал галерею

--- src/views/ContactsView.vue
+++ src/views/ContactsView.vue
@@ -1,10 +1,12 @@
 <template>
+  <div class="wrapper">
   <div class="content">
     <p class="p-cont"><b>ТПК Гласс-Пром</b></p>
     <p>Адрес: ул. Текучёва, 162, г. Ростов-на-Дону</p>
     <p>Телефон: +79045022303</p>
     <p>E-mail: lidiya.yasyrckina@gmail.com</p>
     <p class="p-cont-last"></p>
+  </div>
   </div>
   <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5418.7932360995865!2d39.68950112546697!3d47.228387352374384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40e3b900f2bb7e93%3A0x4ba0cb2ded88067e!2z0KLQn9CaINCT0LvQsNGB0YEt0J_RgNC-0Lw!5e0!3m2!1sru!2sru!4v1652119119257!5m2!1sru!2sru" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 </template>
